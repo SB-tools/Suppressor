27aaf15945d0d690071e75b4710e638be82ff020
caneleex <git@mlnr.dev>
2021-09-18 17:48:09 +0200

    go

---
+++ go.mod
@@ -0,0 +1,10 @@
+module suppressor
+
+go 1.17
+
+require (
+	github.com/DisgoOrg/disgo v0.5.12-0.20210918154309-0a2a89c680df // indirect
+	github.com/DisgoOrg/log v1.1.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ suppressor.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"github.com/DisgoOrg/disgo/core"
+	"github.com/DisgoOrg/disgo/discord"
+	"github.com/DisgoOrg/disgo/gateway"
+	"github.com/DisgoOrg/disgo/rest"
+	"github.com/DisgoOrg/log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	token                = os.Getenv("suppressor")
+	vipRoleId            = discord.Snowflake("755511470305050715")
+	submissionsChannelId = discord.Snowflake("655247785561554945")
+)
+
+func main() {
+	log.SetLevel(log.LevelInfo)
+
+	disgo, err := core.NewBotBuilder(token).
+		SetGatewayConfig(gateway.Config{
+			GatewayIntents: discord.GatewayIntentGuildMessageReactions | discord.GatewayIntentGuildMessages,
+		}).
+		AddEventListeners(&core.ListenerAdapter{
+			OnGuildMessageReactionAdd: onReaction,
+			OnGuildMessageCreate: onMessage,
+		}).SetCacheConfig(core.CacheConfig{
+			MemberCachePolicy: core.MemberCachePolicyNone,
+			CacheFlags: core.CacheFlagsNone,
+		}).
+		Build()
+
+	if err != nil {
+		log.Fatal("error while building disgo: ", err)
+		return
+	}
+
+	defer disgo.Close()
+
+	err = disgo.Connect()
+	if err != nil {
+		log.Fatal("error while starting disgo: ", err)
+	}
+
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-s
+}
+
+func onReaction(event *core.GuildMessageReactionAddEvent) {
+	channelId := event.ChannelID
+	if event.Emoji.Name == "\u2705" && channelId == submissionsChannelId && isVip(event.Member) {
+		channelService := event.Bot().RestServices.ChannelService()
+		suppressed := discord.MessageFlagSuppressEmbeds
+		_, _ = channelService.UpdateMessage(channelId, event.MessageID, discord.MessageUpdate{
+			Flags: &suppressed,
+		})
+	}
+}
+
+func onMessage(event *core.GuildMessageCreateEvent) {
+	if len(event.Message.Stickers) != 0 && !isVip(event.Message.Member) {
+		_ = event.Message.Delete(rest.WithReason("Stickers are not allowed"))
+	}
+}
+
+func isVip(member *core.Member) bool {
+	roles := member.RoleIDs
+	for _, roleId := range roles {
+		if roleId == vipRoleId {
+			return true
+		}
+	}
+	return false
+}
