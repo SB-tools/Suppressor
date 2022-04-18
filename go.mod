4b139c8df663e8b3505f3cc1ce2adef0c1f2eeec
caneleex <git@mlnr.dev>
2022-04-18 16:16:19 +0200

    update disgo

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.17
 // +heroku goVersion go1.17
 
 require (
-	github.com/DisgoOrg/disgo v0.5.12-0.20210927100611-936ea2c0659a
-	github.com/DisgoOrg/log v1.1.2
+	github.com/disgoorg/disgo v0.8.9
+	github.com/disgoorg/log v1.2.0
+	github.com/disgoorg/snowflake v1.1.0
 )
 
 require (
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/sasha-s/go-csync v0.0.0-20210812194225-61421b77c44b // indirect
+	golang.org/x/exp v0.0.0-20220325121720-054d8573a5d8 // indirect
 )
--- suppressor.go
+++ suppressor.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"github.com/DisgoOrg/disgo/core"
-	"github.com/DisgoOrg/disgo/discord"
-	"github.com/DisgoOrg/disgo/gateway"
-	"github.com/DisgoOrg/log"
+	"context"
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/cache"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+	"github.com/disgoorg/log"
+	"github.com/disgoorg/snowflake"
 	"os"
 	"os/signal"
 	"regexp"
@@ -21,14 +26,12 @@ var (
 	regexes []*regexp.Regexp
 
 	commands = []discord.ApplicationCommandCreate{
-		{
-			Type:              discord.ApplicationCommandTypeSlash,
-			Name:              "down",
+		discord.SlashCommandCreate{
+			CommandName:       "down",
 			Description:       "sets whether the server is down (enables wordlist checking)",
 			DefaultPermission: true,
 			Options: []discord.ApplicationCommandOption{
-				{
-					Type:        discord.ApplicationCommandOptionTypeBoolean,
+				discord.ApplicationCommandOptionBool{
 					Name:        "down",
 					Description: "whether the server is down",
 					Required:    false,
@@ -36,35 +39,42 @@ var (
 			},
 		},
 	}
-	guildId   = discord.Snowflake("603643120093233162")
-	vipRoleId = discord.Snowflake("755511470305050715")
+	guildId   = snowflake.Snowflake("603643120093233162")
+	vipRoleId = snowflake.Snowflake("755511470305050715")
 )
 
 func main() {
 	log.SetLevel(log.LevelInfo)
+	log.Info("starting the bot...")
+	log.Info("disgo version: ", disgo.Version)
 
-	disgo, err := core.NewBotBuilder(token).
-		SetGatewayConfigOpts(gateway.WithGatewayIntents(discord.GatewayIntentGuildMessages, discord.GatewayIntentGuildMessageReactions,
-			discord.GatewayIntentGuilds)).
-		SetCacheConfigOpts(
-			core.WithCacheFlags(core.CacheFlagTextChannels),
-			core.WithMemberCachePolicy(core.MemberCachePolicyNone),
-		).
-		AddEventListeners(&core.ListenerAdapter{
-			OnGuildMessageReactionAdd: onReaction,
-			OnGuildMessageCreate:      onMessage,
-			OnSlashCommand:            onSlashCommand,
-		}).
-		Build()
+	client, err := disgo.New(token,
+		bot.WithGatewayConfigOpts(gateway.WithGatewayIntents(discord.GatewayIntentGuildMessages, discord.GatewayIntentGuildMessageReactions,
+			discord.GatewayIntentGuilds)),
+		bot.WithCacheConfigOpts(
+			cache.WithCacheFlags(cache.FlagGuildTextChannels),
+			cache.WithMemberCachePolicy(cache.MemberCachePolicyNone),
+		),
+		bot.WithEventListeners(&events.ListenerAdapter{
+			OnGuildMessageReactionAdd:       onReaction,
+			OnGuildMessageCreate:            onMessage,
+			OnApplicationCommandInteraction: onSlashCommand,
+		}))
 
 	if err != nil {
 		log.Fatal("error while building disgo: ", err)
 		return
 	}
 
-	defer disgo.Close()
+	defer client.Close(context.TODO())
 
-	_, err = disgo.SetGuildCommands(guildId, commands)
+	err = client.ConnectGateway(context.TODO())
+	if err != nil {
+		log.Fatalf("error while connecting to the gateway: %s", err)
+		return
+	}
+
+	_, err = client.Rest().Applications().SetGuildCommands(client.ApplicationID(), guildId, commands)
 	if err != nil {
 		log.Fatalf("error while registering commands: %s", err)
 	}
@@ -73,41 +83,37 @@ func main() {
 		regexes = append(regexes, regexp.MustCompile(variant))
 	}
 
-	err = disgo.ConnectGateway()
-	if err != nil {
-		log.Fatal("error while starting disgo: ", err)
-	}
-
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-s
 }
 
-func onReaction(event *core.GuildMessageReactionAddEvent) {
+func onReaction(event *events.GuildMessageReactionAddEvent) {
 	emoji := event.Emoji.Name
-	if (emoji == "\u2705" || emoji == "\u274C") && isVip(event.Member) {
+	if (emoji == "\u2705" || emoji == "\u274C") && isVip(&event.Member) {
 		suppressed := discord.MessageFlagSuppressEmbeds
-		channelService := event.Bot().RestServices.ChannelService()
+		channelService := event.Client().Rest().Channels()
 		_, _ = channelService.UpdateMessage(event.ChannelID, event.MessageID, discord.MessageUpdate{
 			Flags: &suppressed,
 		})
 	}
 }
 
-func onMessage(event *core.GuildMessageCreateEvent) {
+func onMessage(event *events.GuildMessageCreateEvent) {
 	message := event.Message
 	isVip := isVip(message.Member)
+	channelsRest := event.Client().Rest().Channels()
 	if len(message.Stickers) != 0 && !isVip {
-		_ = message.Delete()
+		_ = channelsRest.DeleteMessage(message.ChannelID, message.ID)
 		return
 	}
-	if !down || message.Author.IsBot || isVip {
+	if !down || message.Author.Bot || isVip {
 		return
 	}
 	content := strings.ToLower(message.Content)
 	for _, regex := range regexes {
 		if regex.MatchString(content) {
-			_, _ = event.Channel().CreateMessage(core.NewMessageCreateBuilder().
+			_, _ = channelsRest.CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().
 				SetContent("SponsorBlock is down at the moment. Stay updated at <https://sponsorblock.works>").
 				Build())
 			return
@@ -115,31 +121,32 @@ func onMessage(event *core.GuildMessageCreateEvent) {
 	}
 }
 
-func onSlashCommand(event *core.SlashCommandEvent) {
-	if event.CommandName == "down" {
-		messageBuilder := core.NewMessageCreateBuilder()
-		downOption := event.Options.Get("down")
-		if downOption == nil {
-			_ = event.Create(messageBuilder.
+func onSlashCommand(event *events.ApplicationCommandInteractionEvent) {
+	data := event.SlashCommandInteractionData()
+	if data.CommandName() == "down" {
+		messageBuilder := discord.NewMessageCreateBuilder()
+		downOption, ok := data.BoolOption("down")
+		if !ok {
+			_ = event.CreateMessage(messageBuilder.
 				SetContentf("The server is currently treated as **%s**.", formatStatus(down)).
 				Build())
 			return
 		}
-		if !isVip(event.Member) {
-			_ = event.Create(messageBuilder.
+		if !isVip(&event.Member().Member) {
+			_ = event.CreateMessage(messageBuilder.
 				SetContent("This command is VIP only.").
 				SetEphemeral(true).
 				Build())
 			return
 		}
-		down = downOption.Bool()
-		_ = event.Create(messageBuilder.
+		down = downOption.Value
+		_ = event.CreateMessage(messageBuilder.
 			SetContentf("The server is now treated as **%s**.", formatStatus(down)).
 			Build())
 	}
 }
 
-func isVip(member *core.Member) bool {
+func isVip(member *discord.Member) bool {
 	roles := member.RoleIDs
 	for _, roleId := range roles {
 		if roleId == vipRoleId {
